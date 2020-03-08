4d123a42549986e68704b9a306c91fe14739caa8
kubesure <edakghar@gmail.com>
2020-03-08 22:01:46 +0400

    initial commit

---
+++ src/main/java/io.kubesure.sidecar.security.data/App.java
@@ -0,0 +1,11 @@
+package io.kubesure.sidecar.security.data;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testCustomerData(){
+        assertTrue("test", true);        
+    }
+}
